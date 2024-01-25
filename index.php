1ff30750dfd050f48b0f8edbfe4288a79ae822c0
Omar Talbi <talbiomar2012@gmail.com>
2024-01-25 12:02:00 +0100

    Add database connection code

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-include 'config.php';
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "projectCloudDB";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 // Create
 if (isset($_POST['submit'])) {
